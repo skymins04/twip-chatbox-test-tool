026d5147599b3ed54868a64d645169ddc2507c94
skymins04 <skymin0417@gmail.com>
2022-06-19 05:44:31 +0900

    기본적인 구조 완성

--- src/background.ts
+++ src/background.ts
@@ -1,31 +1,27 @@
 const intervals: any = {};
+const chatControls: any = {};
 
 class CustomInterval {
-  constructor(callback: () => void, intervalTime: number, randomMax?: number) {
+  constructor(callback: () => void, senderId: string) {
     this.callback = callback;
-    this.intervalTime = intervalTime;
-    if (randomMax) {
-      this.randomFlag = true;
-      this.randomMax = randomMax;
-    }
+    this.senderId = senderId;
     this.set();
   }
 
   private callback: () => void;
-  private randomFlag: boolean = false;
-  private randomMax: number = 0;
-  private intervalTime: number;
+  private senderId: string;
   private timer: NodeJS.Timeout | null = null;
 
   private set() {
     this.timer = setTimeout(
-      () => {
-        this.callback();
-        this.set();
+      async () => {
+        await this.callback();
+        await this.set();
       },
-      this.randomFlag
-        ? this.intervalTime + this.randomMax * Math.random()
-        : this.intervalTime
+      chatControls[this.senderId].offsetFlag
+        ? chatControls[this.senderId].intervalTime +
+            chatControls[this.senderId].offsetTime * Math.random()
+        : chatControls[this.senderId].intervalTime
     );
   }
 
@@ -34,19 +30,109 @@ class CustomInterval {
   }
 }
 
+interface TwitchBadge {
+  broadcaster?: string;
+  moderator?: string;
+  partner?: string;
+  subcriber?: string;
+}
+
+interface TwitchUser {
+  badges: TwitchBadge | null;
+  "badges-raw": string | null;
+  color: string | null;
+  "display-name": string;
+  emotes: Object | null;
+  "emotes-raw": string | null;
+  "first-msg": boolean;
+  id: string;
+  "message-type": string;
+  mod: boolean;
+  "returning-chatter": boolean;
+  "room-id": string;
+  subscriber: boolean;
+  turbo: boolean;
+  "user-id": string;
+  "user-type": string | null;
+  username: string;
+}
+
+chrome.alarms.create({ periodInMinutes: 4.9 });
+chrome.alarms.onAlarm.addListener(() => {
+  console.log("log for debug");
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (sender.id) {
-    const senderId = sender.id;
+    if (request.type === "tool-start") {
+      chrome.scripting.executeScript({
+        target: {
+          tabId: request.tabId as number,
+        },
+        func: () => {
+          (window["testuser" as any] as any) = {
+            badges: null,
+            "badges-raw": null,
+            color: null,
+            "display-name": "testuser",
+            emotes: null,
+            "emotes-raw": null,
+            "first-msg": false,
+            id: "testid",
+            "message-type": "chat",
+            mod: false,
+            "returning-chatter": false,
+            "room-id": "testid",
+            subscriber: false,
+            turbo: false,
+            "user-id": "tesetuser",
+            "user-type": null,
+            username: "testuser",
+          };
+        },
+        world: "MAIN",
+      });
+    } else if (request.type === "chat-control") {
+      const senderId = sender.id;
 
-    if (request.runningState) {
-      console.log("hello");
-      const interval = new CustomInterval(() => {
-        console.log("hello world");
-      }, 2000);
-      intervals[senderId] = interval;
-    } else {
-      console.log("world");
-      intervals[senderId].clear();
+      chatControls[senderId] = {
+        intervalTime: request.intervalTime,
+        offsetTime: request.randomOffset,
+        offsetFlag: request.randomFlag,
+      };
+      if (request.runningState) {
+        if (!intervals[senderId]) {
+          const interval = new CustomInterval(async () => {
+            chrome.scripting.executeScript({
+              target: {
+                tabId: request.tabId as number,
+              },
+              func: () => {
+                (window as any).ChatBox.processMessage(
+                  null,
+                  window["testuser" as any],
+                  "hello world"
+                );
+              },
+              world: "MAIN",
+            });
+          }, senderId);
+          intervals[senderId] = interval;
+        }
+      } else {
+        intervals[senderId].clear();
+        intervals[senderId] = null;
+      }
+    } else if (request.type === "clear-chat") {
+      chrome.scripting.executeScript({
+        target: {
+          tabId: request.tabId as number,
+        },
+        func: () => {
+          (window as any).document.getElementById("log").innerHTML = "";
+        },
+        world: "MAIN",
+      });
     }
   }
 });
--- src/popup.ts
+++ src/popup.ts
@@ -1,16 +1,37 @@
-let store = {
+let store: any = {
   tabActivate: false,
   runningState: false,
+  intervalTime: 1000,
+  randomFlag: false,
+  randomOffset: 1000,
 };
-type StoreKeyType = "tabActivate" | "runningState";
+type StoreKeyType =
+  | "tabActivate"
+  | "runningState"
+  | "intervalTime"
+  | "randomFlag"
+  | "randomOffset";
 let isReady = false;
+let tabId: number | undefined;
 const startBtn = $("#start-btn");
+const clearBtn = $("#clear-btn");
+const importBtn = $("#import-btn");
+const exportBtn = $("#export-btn");
+const intervalInput = $("#input-interval");
+const intervalOffsetInput = $("#input-interval-offset");
+const intervalOffsetChk = $("#input-chk-interval-offset");
 
 const getStore = (key: StoreKeyType) => {
   return store[key];
 };
 const setStore = (key: StoreKeyType, value: any) => {
-  if (key === "tabActivate" || key === "runningState") {
+  if (
+    key === "tabActivate" ||
+    key === "runningState" ||
+    key === "intervalTime" ||
+    key === "randomFlag" ||
+    key === "randomOffset"
+  ) {
     store[key] = value;
     localStorage.setItem("store", JSON.stringify(store));
   }
@@ -21,28 +42,42 @@ $(document).on("DOMContentLoaded", (event) => {
   if (data) {
     store = JSON.parse(data);
   }
-  setInterval(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentURL = tabs[0].url;
-      if (currentURL?.match(/^https:\/\/twip\.kr\/widgets\/chatbox\/.+$/)) {
-        setStore("tabActivate", true);
-        $("#state-msg").html("current tab is twip chatbox");
-        startBtn.removeClass("blocked");
-        $(".btns").addClass("active");
-        isReady = true;
-      } else {
-        setStore("tabActivate", false);
-        $("#state-msg").html("current tab isn't twip chatbox");
-        startBtn.addClass("blocked");
-        $(".btns").removeClass("active");
-        isReady = false;
-      }
-    });
-  }, 10);
+  if (getStore("runningState")) {
+    $("#start-btn").addClass("on");
+    $("#start-btn").removeClass("off");
+  } else {
+    $("#start-btn").addClass("off");
+    $("#start-btn").removeClass("on");
+  }
+  intervalInput.val(parseInt(getStore("intervalTime")));
+  intervalOffsetInput.val(parseInt(getStore("randomOffset")));
+  intervalOffsetChk.prop("checked", getStore("randomFlag"));
+  if (getStore("randomFlag")) {
+    intervalOffsetInput.prop("disabled", false);
+  } else {
+    intervalOffsetInput.prop("disabled", true);
+  }
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentURL = tabs[0].url;
+    tabId = tabs[0].id;
+    if (currentURL?.match(/^https:\/\/twip\.kr\/widgets\/chatbox\/.+$/)) {
+      setStore("tabActivate", true);
+      $("#state-msg").html("current tab is twip chatbox");
+      startBtn.removeClass("blocked");
+      $(".btns").addClass("active");
+      isReady = true;
+      chrome.runtime.sendMessage({ type: "tool-start", tabId });
+    } else {
+      setStore("tabActivate", false);
+      $("#state-msg").html("current tab isn't twip chatbox");
+      startBtn.addClass("blocked");
+      $(".btns").removeClass("active");
+      isReady = false;
+    }
+  });
 });
 
-startBtn.on("click", async (event) => {
-  console.log(getStore("runningState"), isReady, startBtn.hasClass("off"));
+startBtn.on("click", (event) => {
   if (!getStore("runningState") && isReady && startBtn.hasClass("off")) {
     setStore("runningState", true);
     startBtn.removeClass("off");
@@ -53,5 +88,34 @@ startBtn.on("click", async (event) => {
     startBtn.addClass("off");
   }
 
-  chrome.runtime.sendMessage({ runningState: getStore("runningState") });
+  chrome.runtime.sendMessage({ type: "chat-control", ...store, tabId });
+});
+
+clearBtn.on("click", (event) => {
+  chrome.runtime.sendMessage({ type: "clear-chat", tabId });
+});
+
+intervalInput.on("change", (event) => {
+  let val = parseInt($(event.target).val() as string);
+  if (val < 10) val = 10;
+
+  setStore("intervalTime", val);
+  chrome.runtime.sendMessage({ type: "chat-control", ...store, tabId });
+});
+intervalOffsetChk.on("change", (event) => {
+  const flag = $(event.target).prop("checked");
+  setStore("randomFlag", flag);
+  if (flag) {
+    intervalOffsetInput.prop("disabled", false);
+  } else {
+    intervalOffsetInput.prop("disabled", true);
+  }
+  chrome.runtime.sendMessage({ type: "chat-control", ...store, tabId });
+});
+intervalOffsetInput.on("change", (event) => {
+  let val = parseInt($(event.target).val() as string);
+  if (val < 10) val = 10;
+
+  setStore("randomOffset", val);
+  chrome.runtime.sendMessage({ type: "chat-control", ...store, tabId });
 });
