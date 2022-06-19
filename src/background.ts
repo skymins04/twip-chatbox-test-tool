ac1126557261e9fc322cd943f231177ba96f48fd
skymins04 <skymin0417@gmail.com>
2022-06-19 21:25:13 +0900

    랜덤 메시지 & 유저 구현

--- src/background.ts
+++ src/background.ts
@@ -70,25 +70,169 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           tabId: request.tabId as number,
         },
         func: () => {
-          (window["testuser" as any] as any) = {
-            badges: null,
-            "badges-raw": null,
-            color: null,
-            "display-name": "testuser",
-            emotes: null,
-            "emotes-raw": null,
-            "first-msg": false,
-            id: "testid",
-            "message-type": "chat",
-            mod: false,
-            "returning-chatter": false,
-            "room-id": "testid",
-            subscriber: false,
-            turbo: false,
-            "user-id": "tesetuser",
-            "user-type": null,
-            username: "testuser",
-          };
+          (window["testUsers" as any] as any) = [
+            {
+              badges: null,
+              "badges-raw": null,
+              color: null,
+              "display-name": "testuser1",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testuser1",
+              "user-type": null,
+              username: "testuser1",
+            },
+            {
+              badges: null,
+              "badges-raw": null,
+              color: null,
+              "display-name": "testuser2",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testuser2",
+              "user-type": null,
+              username: "testuser2",
+            },
+            {
+              badges: {
+                broadcaster: "1",
+              },
+              "badges-raw": "broadcaster/1",
+              color: null,
+              "display-name": "testbroadcaster",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testbroadcaster",
+              "user-type": null,
+              username: "testbroadcaster",
+            },
+            {
+              badges: {
+                moderator: "1",
+              },
+              "badges-raw": "moderator/1",
+              color: null,
+              "display-name": "testmoderator",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testmoderator",
+              "user-type": null,
+              username: "testmoderator",
+            },
+            {
+              badges: {
+                partner: "1",
+              },
+              "badges-raw": "partner/1",
+              color: null,
+              "display-name": "testpartner",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testpartner",
+              "user-type": null,
+              username: "testpartner",
+            },
+            {
+              badges: {
+                turbo: "1",
+              },
+              "badges-raw": "turbo/1",
+              color: null,
+              "display-name": "testturbo",
+              emotes: null,
+              "emotes-raw": null,
+              "first-msg": false,
+              id: "testid",
+              "message-type": "chat",
+              mod: false,
+              "returning-chatter": false,
+              "room-id": "testid",
+              subscriber: false,
+              turbo: false,
+              "user-id": "testturbo",
+              "user-type": null,
+              username: "testturbo",
+            },
+          ];
+          (window["testMsgs" as any] as any) = [
+            { msg: "Hello World", emotes: null },
+            { msg: "Hi There~~~!", emotes: null },
+            { msg: "트하트하트하트하 유하유하유하유하", emotes: null },
+            {
+              msg: "It's a sample chat test~ blah blah blah blah blah",
+              emotes: null,
+            },
+            {
+              msg: "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세",
+              emotes: null,
+            },
+            {
+              msg: "남산 위에 저 소나무 철갑을 두른 듯 바람 서리 불변함은 우리 기상일세",
+              emotes: null,
+            },
+            { msg: "간장 공장 공장장은 강 공장장이다.", emotes: null },
+            {
+              msg: "트수만 믿고 있으라구 a a b b",
+              emotes: {
+                69: ["12-12", "14-14"],
+                86: ["16-16", "18-18"],
+              },
+            },
+          ];
+
+          (window["getTestUser" as any] as any) = () =>
+            (window["testUsers" as any] as any)[
+              Math.floor(
+                Math.random() * (window["testUsers" as any] as any).length
+              )
+            ];
+
+          (window["getTestMsg" as any] as any) = () =>
+            (window["testMsgs" as any] as any)[
+              Math.floor(
+                Math.random() * (window["testMsgs" as any] as any).length
+              )
+            ];
         },
         world: "MAIN",
       });
@@ -108,11 +252,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 tabId: request.tabId as number,
               },
               func: () => {
-                (window as any).ChatBox.processMessage(
-                  null,
-                  window["testuser" as any],
-                  "hello world"
-                );
+                let user = (window["getTestUser" as any] as any)();
+                let msgData = (window["getTestMsg" as any] as any)();
+                let rawStr = "";
+                let i = 0;
+                if (msgData.emotes !== null) {
+                  for (let emote in msgData.emotes) {
+                    if (i !== 0) rawStr += "/";
+
+                    let str = emote + ":";
+                    let j = 0;
+                    for (let item of msgData.emotes[emote]) {
+                      if (j !== 0) str += ",";
+                      str += item;
+                      j += 1;
+                    }
+                    rawStr += str;
+                    i += 1;
+                  }
+                  user["emotes-raw"] = rawStr;
+                  console.log(rawStr);
+                }
+                user["emotes"] = msgData.emotes;
+                console.log(user["emotes"]);
+                (window as any).ChatBox.processMessage(null, user, msgData.msg);
               },
               world: "MAIN",
             });
